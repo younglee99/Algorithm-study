f76824351224df0c25367814e8e75080c3b6073b
younglee99 <102978288+younglee99@users.noreply.github.com>
2023-12-07 22:43:44 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for(int i=0; i<commands.size(); i++){
+        vector<int>number;
+        for(int j=commands[i][0]-1; j<commands[i][1]; j++){
+            number.push_back(array[j]);
+        }
+        sort(number.begin(),number.end());
+        answer.push_back(number[commands[i][2]-1]);
+    }
+    
+    return answer;
+}
