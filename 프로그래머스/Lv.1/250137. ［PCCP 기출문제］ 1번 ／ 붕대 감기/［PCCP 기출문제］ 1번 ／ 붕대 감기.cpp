2e33c6f113858d6782bb1b8e2fa6c9ccecbf6540
younglee99 <102978288+younglee99@users.noreply.github.com>
2024-01-12 22:42:26 +0900

    [level Lv.1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.11 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> bandage, int health, vector<vector<int>> attacks) {
+    int answer = 0;
+    return answer;
+}
