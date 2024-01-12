3eec38557c0c1eff0e936930fbfe83d74f0da2c6
younglee99 <102978288+younglee99@users.noreply.github.com>
2024-01-13 02:53:42 +0900

    [level Lv.2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 3.65 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/Lv.2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
+++ 프로그래머스/Lv.2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
@@ -1,71 +1,56 @@
 #include <string>
 #include <vector>
+#include <iostream>
 #include <queue>
-#include <cstring>
-#include <map>
+#include <algorithm>
 #include <set>
+#include <map>
 
 using namespace std;
-
+queue<pair<int,int>>q;
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int visited[510][510] = {0};
+int n, m =0;
+int index = 1;
+int result[501];
 vector<vector<int>> maps;
-map<int, int> cnt;
-int visited[501][501];
-int n, m;
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
-int depth = 1;
 
-void BFS(int sx, int sy){
-    int tmp = 0;
-    queue<pair<int, int>> q;
-    q.push({sx, sy});
-    maps[sx][sy] = depth;
-    visited[sx][sy] = 1;
-    
+void bfs(int x, int y){
+    int cnt = 0;
+    set<int>tmp;
+    q.push({x,y});
+    visited[x][y] = index;
+    tmp.insert(y);
     while(!q.empty()){
-        int x = q.front().first;
-        int y = q.front().second;
-        tmp++;
+        int first = q.front().first;
+        int second = q.front().second;
+        cnt++;
         q.pop();
-        
         for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] || maps[nx][ny] == 0) continue;
-            maps[nx][ny] = depth;
-            visited[nx][ny] = 1;
-            q.push({nx, ny});
+            int nx = first + dx[i];
+            int ny = second + dy[i];
+            if(nx<0 || nx>=n || ny<0 || ny>=m || maps[nx][ny] == 0 || visited[nx][ny]) continue;
+            q.push({nx,ny});
+            visited[nx][ny] = index;
+            tmp.insert(ny);
         }
     }
-    
-    cnt[depth++] = tmp; // depth번째 석유덩어리가 갖는 석유 총량
+    for(auto ss : tmp) result[ss] += cnt;
 }
 
 int solution(vector<vector<int>> land) {
     int answer = 0;
-    n = land.size(); // 열의 길이
-    m = land[0].size(); // 행의 길이
-    memset(visited, 0, sizeof(visited)); // 방문 초기화
-    
+    n = land.size();
+    m = land[0].size();
     maps = land;
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            if(visited[i][j] == 0 && maps[i][j] > 0) 
-                BFS(i, j);
+            if(land[i][j] == 1 && visited[i][j]==0) {
+                bfs(i, j);
+            }
         }
     }
-    
-    for(int i=0; i<m; i++){
-        int temp = 0;
-        set<int> s; // set으로 어떤 석유덩어리를 얻었는지 확인
-        for(int j=0; j<n; j++){
-            s.insert(maps[j][i]);
-        }
-        for(auto it: s){
-            temp += cnt[it];
-        }
-        answer = max(answer, temp);
-    }
+    for(int i=0; i<m; i++) answer = max(answer, result[i]);
     return answer;
 }
