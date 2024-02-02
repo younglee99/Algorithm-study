b425f47a2ca77c4b4c7e0ef89994f03d2221b6d3
younglee99 <102978288+younglee99@users.noreply.github.com>
2024-02-02 22:47:46 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	double a, b;
+	cin >> a >> b;
+	cout.precision(9); 
+	cout << fixed; 
+	cout << a / b;
+
+
+}
