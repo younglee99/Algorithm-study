44c945ce4f02643e553fffd5601ffd08476de43b
younglee99 <102978288+younglee99@users.noreply.github.com>
2024-02-08 01:09:24 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 4 ms, Memory: 4524 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.cc
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.cc
@@ -3,90 +3,85 @@
 #include <algorithm>
 
 using namespace std;
-
 int n, m;
 int map[550][550];
-vector<int> d;
-vector<int> s;
-int dx[8] = { 0, -1, -1, -1, 0, 1, 1, 1 };
-int dy[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
-int fx[4] = { -1, -1, 1, 1 };
-int fy[4] = { -1, 1, -1, 1 };
-vector<pair<int, int>> cloud;
+int d[100];
+int s[510];
+int dx[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy[8] = { -1,-1,0,1,1,1,0,-1 };
+int fx[4] = { -1,-1,1,1 };
+int fy[4] = { -1,1,-1,1 };
+vector<pair<int, int>>cloud;
 int visited[550][550];
 
 void move(int di, int num) {
-    for (int i = 0; i < cloud.size(); i++) {
-        int x = cloud[i].first;
-        int y = cloud[i].second;
-        int nx = x + (dx[di] * (num % n));
-        int ny = y + (dy[di] * (num % n));
+	for (int i = 0; i < cloud.size(); i++) {
+		int x = cloud[i].first;
+		int y = cloud[i].second;
+		int nx = x + (dx[di-1]*(num%n));
+		int ny = y + (dy[di-1]*(num%n));
 
-        if (nx < 1) nx += n;
-        if (ny < 1) ny += n;
-        if (nx > n) nx %= n;
-        if (ny > n) ny %= n;
-        cloud[i].first = nx;
-        cloud[i].second = ny;
-        visited[nx][ny] = 1;
-        map[nx][ny]++;
-    }
+		if (nx < 1) nx += n;
+		if (ny < 1) ny += n;
+		if (nx > n) nx %= n;
+		if (ny > n) ny %= n;
+		cloud[i].first = nx;
+		cloud[i].second = ny;
+		visited[nx][ny] = 1;
+		map[nx][ny]++;
+	}
 }
-
 void dup() {
-    for (int i = 0; i < cloud.size(); i++) {
-        int nx = cloud[i].first;
-        int ny = cloud[i].second;
-        int cnt = 0;
-        for (int j = 0; j < 4; j++) {
-            int mx = nx + fx[j];
-            int my = ny + fy[j];
-            if (mx < 1 || mx > n || my < 1 || my > n) continue;
-            if (map[mx][my] > 0) cnt++;
-        }
-        map[nx][ny] += cnt;
-    }
+	for (int i = 0; i < cloud.size(); i++) {
+		int nx = cloud[i].first;
+		int ny = cloud[i].second;
+		int cnt = 0;
+		for (int j = 0; j < 4; j++) {
+			int mx = nx + fx[j];
+			int my = ny + fy[j];
+			if (mx<1 || mx>n || my<1 || my>n) continue;
+			if (map[mx][my] > 0)cnt++;
+		}
+		map[nx][ny] += cnt;
+	}
 }
-
 void create() {
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (visited[i][j] == 0 && map[i][j] > 1) {
-                map[i][j] -= 2;
-                cloud.push_back({ i, j });
-            }
-        }
-    }
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (visited[i][j] == 0 && map[i][j] > 1) {
+				map[i][j] -= 2;
+				cloud.push_back({ i,j });
+			}
+		}
+	}
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    cin >> n >> m;
-    d.resize(m);
-    s.resize(m);
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++) cin >> map[i][j];
-    for (int i = 0; i < m; i++) cin >> d[i] >> s[i];
-    cloud.push_back({ n - 1, 1 });
-    cloud.push_back({ n - 1, 2 });
-    cloud.push_back({ n, 1 });
-    cloud.push_back({ n, 2 });
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++) cin >> map[i][j];
+	for (int i = 0; i < m; i++) cin >> d[i] >> s[i];
+	cloud.push_back({ n - 1,1 });
+	cloud.push_back({ n - 1,2 });
+	cloud.push_back({ n ,1 });
+	cloud.push_back({ n ,2 });
 
-    for (int i = 0; i < m; i++) {
-        for (int p = 1; p <= n; p++)
-            for (int j = 1; j <= n; j++) visited[p][j] = 0;
+	for (int i = 0; i < m; i++) {
+		for (int p = 1; p <= n; p++)
+			for (int j = 1; j <= n; j++) visited[p][j] = 0;
 
-        move(d[i] - 1, s[i]);
-        dup();
-        cloud.clear();
-        create();
-    }
+		move(d[i], s[i]);
+		dup();
+		cloud.clear();
+		create();
 
-    int result = 0;
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++) result += map[i][j];
+	}
+	int result = 0;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++) result += map[i][j];
 
-    cout << result;
+	cout << result;
 }
